87501adf3f53b8832eee0999f3dc16658a7b75e7
malesal <133693731+malesal@users.noreply.github.com>
2023-05-21 20:21:57 -0300

    Update tp38.cpp

--- tp38.cpp
+++ tp38.cpp
@@ -8,13 +8,13 @@ int main(){
     int num;
     cout << "Ingrese un numero: ";
     cin >> num;
-    if(esMultiploDe17(num)) cout << num << " es múltiplo de 17" << endl;
-    else cout << num << " no es múltiplo de 17" << endl;
+    if(esMultiploDe17(num)) cout << num << " es mÃºltiplo de 17" << endl;
+    else cout << num << " no es mÃºltiplo de 17" << endl;
     return 0;
 }
 
 bool esMultiploDe17(int num){
-    if(num % 17 == 0) 
+    if(num % 17 == 0)
 	return true;
     else
 	 return false;
